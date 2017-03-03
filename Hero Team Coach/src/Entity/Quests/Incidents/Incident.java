21c89cc4581d8f0c0fc68ffd17eba7803686cc46
jefbu <Jefbussens@gmail.com>
2017-03-03 20:15:54 +0100

    put combat method in Incident abstract class

--- Hero Team Coach/src/Entity/Quests/Incidents/Incident.java
+++ Hero Team Coach/src/Entity/Quests/Incidents/Incident.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Random;
 
 import Entity.Armour;
+import Entity.Hero;
 import Entity.Mob;
 import Entity.Status;
 import Entity.Weapon;
@@ -65,6 +66,86 @@ public void initiate() {
 	GameScreen.questScreen.textLabel.setText(title);
 	
 	}
+
+public String combat() {
+	
+	Random random = new Random();
+	
+	int heroCombat = 0;
+	int enemyCombat = 0;
+	
+	for (Hero quester: GameScreen.questers) {
+		heroCombat = heroCombat + quester.questCombat;			
+	}
+	
+	heroCombat = heroCombat + random.nextInt(250);
+	
+	for (Mob goblin: enemies) {
+		enemyCombat = enemyCombat + goblin.combat;			
+	}
+	
+	enemyCombat = enemyCombat + random.nextInt(200);
+
+	
+	if (heroCombat < enemyCombat) {
+					
+		int activeCombatants = 0;
+		for (Hero quester: GameScreen.questers) {
+			if (quester.status == Status.HEALTHY) activeCombatants ++;
+		}
+		
+		for (int i = 0; i < activeCombatants; i++) {
+			
+			int roll = random.nextInt(10);
+			if (roll < 2) {
+				
+				int proposedHero = random.nextInt(GameScreen.questers.size());
+				while (GameScreen.questers.get(proposedHero).status != Status.HEALTHY) {
+					proposedHero = random.nextInt(GameScreen.questers.size());
+				}
+
+				
+				GameScreen.questers.get(proposedHero).status = Status.WOUNDED;
+				output = output +  "Hero " + GameScreen.questers.get(proposedHero).name + " was wounded \n";
+			}
+			else if (roll < 5) {
+				
+				int proposedHero = random.nextInt(GameScreen.questers.size());
+				while (GameScreen.questers.get(proposedHero).status != Status.HEALTHY) {
+					proposedHero = random.nextInt(GameScreen.questers.size());
+				}
+				
+				GameScreen.questers.get(proposedHero).questCombat -= random.nextInt(10);
+				output = output + "Hero " + GameScreen.questers.get(proposedHero).name + " was slightly wounded \n";
+
+			}
+			
+		}
+
+	}
+	
+	else if (heroCombat  < enemyCombat * 2) {
+		
+		int activeCombatants = 0;
+		for (Hero quester: GameScreen.questers) {
+			if (quester.status == Status.HEALTHY) activeCombatants ++;
+		}
+		
+		for (int i = 0; i < activeCombatants; i++) {
+			
+			int proposedHero = random.nextInt(GameScreen.questers.size());
+			while (GameScreen.questers.get(proposedHero).status != Status.HEALTHY) {
+				proposedHero = random.nextInt(GameScreen.questers.size());
+			}
+				GameScreen.questers.get(proposedHero).questCombat -= random.nextInt(5) + 1;
+				output = output + "Hero " + GameScreen.questers.get(proposedHero).name + " was slightly wounded \n";
+		}
+		
+	}
+	
+	return "hello world";
+	
+}
 
 
 }
